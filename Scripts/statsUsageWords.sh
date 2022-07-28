b35ebe274081bd6796b5367c5a5b893ca438b65f
Salvador Nicolás Sanchez <salvadorsanchez1832005@gmail.com>
2022-07-28 18:02:09 -0300

    ejersicio casi hecho

--- Scripts/statsUsageWords.sh
+++ Scripts/statsUsageWords.sh
@@ -2,10 +2,42 @@
 
 #mostrar un top 10 de palabras de mas de 4 letras con mas apariciones
 
-for C in $(cat $1); do
-	if [[ ${#C} > 3 ]]; then
-		echo ${#C} $C
+PALABRAS=$(cat $1)
+
+declare -A REPETICIONES
+
+for PALABRA in $(grep -P -o '\b\w+\b' $1); do
+	if [[ ${#PALABRA} > 3 ]]; then
+		let REPETICIONES[$PALABRA]++
 	fi
 done
 
-echo "4 5 2 6"
+declare -A INVERSO
+
+for PALABRA in ${!REPETICIONES[@]}; do
+	INVERSO[${REPETICIONES[$PALABRA]}]+=" $PALABRA"
+done
+
+LISTA=$( echo ${REPETICIONES[@]} | sort | uniq -c | sort -nr | head -10 )
+
+TOP=0
+PROV=""
+
+for E in $LISTA; do
+	if [[ $E != $PROV ]]; then
+		PROV=$E
+		let TOP++
+	fi
+done
+
+echo "--- 10 palabras con mas apariciones en $1 ---"
+echo ""
+
+for E in $LISTA; do
+	if [[ $E != $PROV ]]; then
+		echo "Top $TOP con $E apariciones:${INVERSO[$E]}"
+		echo ""
+		let TOP--
+		PROV=$E
+	fi
+done
