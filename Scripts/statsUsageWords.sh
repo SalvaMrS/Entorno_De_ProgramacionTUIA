39f5d09bb23faab4c25f23ae8322cea70d2783eb
Salvador Nicolás Sanchez <salvadorsanchez1832005@gmail.com>
2022-07-28 21:37:25 -0300

    Final mas que haceptable

--- Scripts/statsUsageWords.sh
+++ Scripts/statsUsageWords.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+#Creamos un diccionario con las palabras como claves y las apariciones como valores
+
 declare -A REPETICIONES
 
 for PALABRA in $(grep -P -o '\b\w+\b' $1); do
+
+	#filtramos las palabras con una longitus menor a 3 caracteres
+
 	if [[ ${#PALABRA} > 3 ]]; then
 		let REPETICIONES[$PALABRA]++
 	fi
 done
+
+#Creamos un diccionario con las apariciones como las claves y las palabras como los valores
 
 declare -A INVERSO
 
@@ -14,7 +21,9 @@ for PALABRA in ${!REPETICIONES[@]}; do
 	INVERSO[${REPETICIONES[$PALABRA]}]+=" $PALABRA"
 done
 
-IFS=$'\n' LISTA=$( sort -r <<< "${REPETICIONES[*]}")
+#Ordenamos las apariciones de mayor a menor
+
+IFS=$'\n' LISTA=$( sort -gr <<< "${REPETICIONES[*]}")
 unset IFS
 
 TOP=1
@@ -23,9 +32,13 @@ echo ""
 echo "--- 10 palabras con mas apariciones en $1 ---"
 echo ""
 
-for E in $LISTA; do
-	echo "Top $TOP con $E apariciones:${INVERSO[$E]}"
-	echo ""
-	let TOP++
-	PROV=$E
+#Imprimimos en consola el top 10 palabras con mas apariciones
+
+for E in ${LISTA[@]};do
+	if [[ ($TOP != 10) && ( $TOP != ${#LISTA} ) ]]; then
+		echo "Top $TOP con $E apariciones: ${INVERSO[$E]}"
+		echo
+		let TOP++
+	fi
 done
+
