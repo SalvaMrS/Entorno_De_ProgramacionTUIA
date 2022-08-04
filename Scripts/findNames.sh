3a9dfd7be69bcff3388b9fadcc3f5fe037207a4e
Salvador Nicolás Sanchez <salvadorsanchez1832005@gmail.com>
2022-08-04 10:54:05 -0300

    findName completado correctamente

--- Scripts/findNames.sh
+++ Scripts/findNames.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+NOMBRE="^[A-Z][a-z]+"
+
 echo
 echo "Lista de nombres propios (Nnn) en el archivo $1:"
 echo
 
 #filtramos del archivo las palabras que inicien en mayusculas y prosigan en minusculas y las imprimimos en consola
-for PAL in $(cat $1 | grep -oE "^[A-Z][a-z]+"); do
-	echo "-$PAL"
+for PAL in $(cat $1); do
+	if [[ $PAL =~ $NOMBRE ]];then
+		echo "-$PAL"
+	fi
 done
