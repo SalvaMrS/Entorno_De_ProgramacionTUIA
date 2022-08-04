286ba85bc88c0ffba6b82eb7badca65293ad0100
Salvador Nicolás Sanchez <salvadorsanchez1832005@gmail.com>
2022-08-04 10:33:11 -0300

    Modificación del archvo Ejemplo

--- Scripts/statsSentences.sh
+++ Scripts/statsSentences.sh
@@ -7,16 +7,15 @@ LCORTO=99999999999
 TOTAL=0
 CANTIDAD=0
 
-SALTO='\n'
 PUNTO='[.]$'
 ORACION=""
 
 #Por cada oracion si es mas larga que LARGO reemplazalo por esta
 #Y si es mas corto que CORTO reemplazalo por esta
 
-for E in $(cat $1); do
+for E in $( cat $1); do
 
-	if [[ $E =~ $PUNTO || $E =~ $SALTO ]]; then
+	if [[ $E =~ $PUNTO ]]; then
 		ORACION="$ORACION $E"
 
 		let TOTAL+=${#ORACION}
@@ -45,8 +44,10 @@ PROMEDIO=$(($TOTAL/$CANTIDAD))
 echo
 echo "Del archivo $1 :"
 echo
-echo "-La oración más larga es \"$LARGO\" con una longitud de $LLARGO caracteres"
+echo "-La oración más larga es: \"$LARGO\" "
+echo "Con una longitud de $LLARGO caracteres"
 echo
-echo "-La oración más corta es \"$CORTO\" con una longitud de $LCORTO caracteres"
+echo "-La oración más corta es: \"$CORTO\" "
+echo "Con una longitud de $LCORTO caracteres"
 echo
 echo "-El promedio de caracteres por oración es de $PROMEDIO"
